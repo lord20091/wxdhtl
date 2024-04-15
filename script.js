29bc413bd5bc60d847ab65a9147cd9b06c51e58f
lord20091 <abdulazizbozorov009@gmail.com>
2024-04-15 15:23:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let randomNum = Math.round(Math.random() * 100);
+
+alert(randomNum);
+
+alert( randomNum + ` ikkiga ko'paytirganligi ` + randomNum * 2 + ` ga teng`);    
+
+alert( randomNum + ` ni ikkiga bo'lingandagi qoldiq ` + (randomNum % 2) + " ga teng");
